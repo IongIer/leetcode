fe3d174ac8d29de9831ccb9355d1487f734dead6
oni <mosionroatarevine@gmail.com>
2022-07-28 16:07:10 +0300

    Small optimization

--- mountainlist.py
+++ mountainlist.py
@@ -5,9 +5,9 @@ def validMountainArray(arr: List[int]) -> bool:
     if len(arr) < 3:
         return False
     peak = max(arr)
-    i = arr.index(peak)
     if peak == arr[-1] or peak == arr[0]:
         return False
+    i = arr.index(peak)
     first = sorted(list(set(arr[: i + 1])))
     second = sorted(list(set(arr[i:])), reverse=True)
     return first == arr[: i + 1] and second == arr[i:]
