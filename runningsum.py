9c9397618800515ff1ea32e550ff77900627e095
oni <mosionroatarevine@gmail.com>
2022-08-02 12:30:26 +0300

    My overlycomplex solution

---
+++ runningsum.py
@@ -0,0 +1,13 @@
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+# Return the running sum of nums.
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        running = []
+        last = sum(nums)
+        for val in nums[::-1]:
+            running.append(last)
+            last -= val
+        return running[::-1]
