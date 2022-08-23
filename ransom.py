3731341c690e016ced5d5680c6b3fd5e0fd5eb90
oni <mosionroatarevine@gmail.com>
2022-08-23 12:27:38 +0300

    comparing two dictionaries

---
+++ ransom.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count_ransom = Counter(ransomNote)
+        count_magazine = Counter(magazine)
+        return count_ransom <= count_magazine
