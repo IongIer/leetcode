0e31ff29ace54bf1de648641b1b1103f72365c2c
oni <mosionroatarevine@gmail.com>
2022-08-25 10:15:59 +0300

    simplified conditionals

--- floodfill.py
+++ floodfill.py
@@ -2,31 +2,27 @@ from typing import List
 
 
 class Solution:
-    def floodFill(
-        self, image: List[List[int]], sr: int, sc: int, color: int
-    ) -> List[List[int]]:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         initial = image[sr][sc]
         row = len(image) - 1
         col = len(image[0]) - 1
-
-        def dfs(image, sr, sc, color):
-            nonlocal row, col, initial
+        def dfs(image, sr, sc):
+            nonlocal row, col, initial, color
             l, r, u, d = sr - 1, sr + 1, sc - 1, sc + 1
-            if image[sr][sc] == initial:
-                image[sr][sc] = color
-                if 0 <= l <= row and 0 <= sc <= col:
-                    if image[l][sc] == initial:
-                        dfs(image, l, sc, color)
-                if 0 <= r <= row and 0 <= sc <= col:
-                    if image[r][sc] == initial:
-                        dfs(image, r, sc, color)
-                if 0 <= sr <= row and 0 <= u <= col:
-                    if image[sr][u] == initial:
-                        dfs(image, sr, u, color)
-                if 0 <= sr <= row and 0 <= d <= col:
-                    if image[sr][d] == initial:
-                        dfs(image, sr, d, color)
+            image[sr][sc] = color
+            if 0 <= l:
+                if image[l][sc] == initial:
+                    dfs(image, l, sc)
+            if r <= row:
+                if image[r][sc] == initial:
+                    dfs(image, r, sc)
+            if 0 <= u:
+                if image[sr][u] == initial:
+                    dfs(image, sr, u)
+            if d <= col:
+                if image[sr][d] == initial:
+                    dfs(image, sr, d)
 
+                
         if initial != color:
-            dfs(image, sr, sc, initial, color)
-        return image
+            dfs(image, sr, sc)
