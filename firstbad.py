783286f77a73734bf1889a7033f275176ced6fab
oni <mosionroatarevine@gmail.com>
2022-08-30 13:10:24 +0300

    this doesn't work yet

---
+++ firstbad.py
@@ -0,0 +1,13 @@
+def firstBadVersion(n) -> int:
+    l = 0
+    r = len(n)-1
+    while l <= r:
+        mid = (l + r) // 2
+        if n[mid]:
+            r = mid
+            bad = mid
+        else:
+            l = mid - 1
+    return bad
+
+print(firstBadVersion([0,0,0,1,1]))
