2d267900d75aaaaa27a57c5a0158280807628a3a
oni <mosionroatarevine@gmail.com>
2022-09-10 11:01:59 +0300

    my first heapq, getting largest instead of smallest by multiplying with -1

---
+++ laststone.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        inverted = [-x for x in stones]
+        heapq.heapify(inverted)
+        while inverted:
+            try:
+                if inverted[0] == inverted[1]:
+                    heapq.heappop(inverted)
+                    heapq.heappop(inverted)
+                else:
+                    first = abs(heapq.heappop(inverted))
+                    second = abs(heapq.heappop(inverted))
+                    heapq.heappush(inverted, -(first - second))
+            except IndexError:
+                return abs(inverted[0])
+        return 0
