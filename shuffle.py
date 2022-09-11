6e680328884675cebd0ee18dc34019e3db10d37d
oni <mosionroatarevine@gmail.com>
2022-09-11 10:30:24 +0300

    Using a generator expression, O(n) space/time

---
+++ shuffle.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        for i in range(n):
+            yield nums[i]
+            yield nums[i + n]
