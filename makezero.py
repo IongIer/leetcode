3c7b8477f1fec02cdaa16cc7f56b0bf15c6edd7d
oni <mosionroatarevine@gmail.com>
2022-09-15 11:34:58 +0300

    fast solve of an easy using set, might not be optimal speed

---
+++ makezero.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
+
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        nonzero = set(n for n in nums if n != 0)
+        return len(nonzero) if nonzero else 0
