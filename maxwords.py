8debf244687d43fd05c1db239be4ed898307a29e
oni <mosionroatarevine@gmail.com>
2022-09-26 10:46:48 +0300

    a simple one

---
+++ maxwords.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        maximum = 0
+        for s in sentences:
+            maximum = max(maximum, len(s.split()))
+        return maximum           
+        
