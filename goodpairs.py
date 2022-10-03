eec75138244a1c93368fce222476f0b4bba7b4d4
oni <mosionroatarevine@gmail.com>
2022-10-03 18:32:32 +0300

    simple one for today

---
+++ goodpairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        answer = 0
+        for c in count:
+            if count[c] > 1:
+                answer += (count[c] * (count[c] - 1)) // 2
+        return answer
