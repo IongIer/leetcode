30a2dd7274527c125fd89b97e50dd1bfaca836d1
ioni <104294646+IongIer@users.noreply.github.com>
2022-11-29 11:17:06 +0200

    O(1) operations for a random set class

---
+++ randomset.py
@@ -0,0 +1,27 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.arr = []
+        self.hash = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.hash:
+            return False
+        self.hash[val] = len(self.arr)
+        self.arr.append(val)
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hash:
+            return False
+        self.hash[self.arr[-1]] = self.hash[val]
+        self.arr[self.hash[val]] = self.arr[-1]
+        self.arr[-1] = val
+        self.hash.pop(val)
+        self.arr.pop()
+        return True
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
