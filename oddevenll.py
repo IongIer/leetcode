d0a165da47152c3e53faf0bd400e0e053fd71197
ioni <104294646+IongIer@users.noreply.github.com>
2022-12-06 17:42:54 +0200

    shuffle a linked list

---
+++ oddevenll.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        current_odd = head
+        current_even = head.next
+        second_half_head = current_even
+        while current_even and current_even.next:
+            current_odd.next, current_even.next = current_odd.next.next, current_even.next.next
+            current_odd, current_even = current_odd.next, current_even.next
+        current_odd.next = second_half_head
+        return head
